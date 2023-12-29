15880bbfb3e4ee180c3448e809a84653034fad6c
Thayna Caixeta <thayna.caixeta@gmail.com>
2023-12-29 17:44:37 +0000

    First commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("1 - Begin monitorization")
+	fmt.Println("2 - Show logs")
+	fmt.Println("3 - Exit")
+
+	var action int
+	fmt.Scanf("%d", &action)
+}
